53c76d34f0cdba2da6779a84afbd280528443ea7
Falkreon <falkreon@gmail.com>
2017-02-15 16:01:50 -0600

    Remove "The" because I'm thorough and make quality commits. /s

--- src/main/java/io/github/elytra/thermionics/Proxy.java
+++ src/main/java/io/github/elytra/thermionics/Proxy.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2017 The Isaac Ellingson (Falkreon) and contributors
+ * Copyright (c) 2017 Isaac Ellingson (Falkreon) and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
